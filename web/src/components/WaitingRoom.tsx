8c29565f7431ac370b150f877bf70ba140500bda
Christopher Geiger <christophergeiger3@gmail.com>
2021-01-19 17:27:40 -0500

    Add spinner to WaitingRoom

--- web/src/components/WaitingRoom.tsx
+++ web/src/components/WaitingRoom.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { useMeQuery } from "../generated/graphql";
+import { Spinner } from "@chakra-ui/react"
 
 export const WaitingRoom = ({ code }) => {
   const { data, loading, error } = useMeQuery();
 
+  if(loading) return (
+      <span>{loading ? <Spinner /> : <div></div>}</span>
+  )
+
   return (
     <div>
-      <span>{loading ? <p>Loading ...</p> : <div></div>}</span>
       <span>{(data !== null && !!data) ? data.me.players.join(", ") : "empty!"}</span>
     </div>
   );
