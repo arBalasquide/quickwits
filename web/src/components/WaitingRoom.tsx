39a6eb087dfa4b6f481ed0a69d34a010e439f841
arBalasquide <adrian.balasquide@gmail.com>
2021-01-25 12:10:47 -0400

    Implemented subscription component

--- web/src/components/WaitingRoom.tsx
+++ web/src/components/WaitingRoom.tsx
@@ -1,52 +1,10 @@
-import { gql, useQuery } from "@apollo/client";
 import React, { useState } from "react";
+import { useOnNewPlayerSubscription } from "../generated/graphql";
 
 export const WaitingRoom = ({}) => {
-  const [players, setPlayers] = useState([]);
-  const {loading, error, data } = useQuery(gql`query Me {
-    me {
-      game_code
-      players
-      owner
-    }
-  } `, {
-    pollInterval: 5000
-  })
+  const { data, loading, error } = useOnNewPlayerSubscription();
   
-  if (loading) return <div>"loading...";</div>
-  if (error) return <div>`Error: ${error}`</div>
-
-  if(!!data && !!data.me) { 
-    for(var i=0; i < data.me.players.length; i++) {
-      if(players[i] !== data.me.players[i])
-        setPlayers(data.me.players);
-    }
-  }
-  
-  console.log(`Players: ${JSON.stringify(data)}`)
-
-  return <div>{!!players ? players.join(",") : "No one has joined yet."}</div>;
+  return <div>{!!data ? data.newPlayer.username : "no players yet"}</div>;
 };
 
 export default WaitingRoom;
-
-// import React, { useEffect, useState } from "react";
-// import { socket } from "../service/socket";
-// import getGameCode from "../utils/getGameCode";
-
-// export const WaitingRoom = ({code}) => {
-//   const [players, setPlayers] = useState([]);
-
-//   useEffect(() => {
-//     socket.emit("getPlayers", 100, code);
-    
-//     socket.on("players", (data) => {
-//       setPlayers(data);
-//     });
-//   }, []);
-
-//   return <div>{!!players ? players.join(",") : "No one has joined yet."}</div>;
-// };
-
-// export default WaitingRoom;
-
