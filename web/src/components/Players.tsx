94b72a9d70711a0aad49863c9723927b71c31f32
arBalasquide <adrian.balasquide@gmail.com>
2021-01-26 12:17:06 -0400

    Added some styling for usernames

---
+++ web/src/components/Players.tsx
@@ -0,0 +1,30 @@
+import {Container, Grid, Text, HStack, Circle, Center} from "@chakra-ui/react"
+
+const createCirles = (players: [string]) => {
+    let body = [];
+    for (var i = 0; i < players.length; i++) {
+        body.push(<Circle size="200px" bg="tomato" m={2}><Text fontSize="1.5em" color="blue.900" isTruncated>{players[i]}</Text></Circle>);
+    }
+
+    return (
+        <Center>
+            {body}
+        </Center>
+    );
+}
+
+export const Players = ({players}) => {
+    return (
+        <Container maxW={"80%"}>
+            <Grid>
+                <Center>
+                    <HStack>
+                        {createCirles(players)}
+                    </HStack>
+                </Center>
+            </Grid>
+        </Container>
+    )
+}
+
+export default Players
--- web/src/components/WaitingRoom.tsx
+++ web/src/components/WaitingRoom.tsx
@@ -1,37 +1,39 @@
-import { useSubscription } from "@apollo/client";
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import {
-  useOnNewPlayerSubscription,
-  usePlayerQuery,
+    useOnNewPlayerSubscription,
+    usePlayerQuery,
 } from "../generated/graphql";
+import Players from "./Players";
 
 export const WaitingRoom = ({}) => {
-  const [game_code, setGameCode] = useState("");
-  const [players, setPlayers] = useState([]);
+    const [game_code, setGameCode] = useState("");
+    const [players, setPlayers] = useState([]);
 
-  const player = useOnNewPlayerSubscription({
-    variables: {
-      game_code: game_code,
-    },
-  });
+    const player = useOnNewPlayerSubscription({
+        variables: {
+            game_code: game_code,
+        },
+    });
 
-  const {data, loading, error} = usePlayerQuery();
+    const {data, loading} = usePlayerQuery();
 
-  useEffect(() => {
-    if(data && data.player){
-      setGameCode(data.player.game_code);
+    useEffect(() => {
+        if (data && data.player) {
+            setGameCode(data.player.game_code);
+        }
+        if (player && player.data) {
+            players.push(player.data.newPlayer.username)
+            setPlayers(players)
+        }
+    }, [data, player])
+
+    if (loading) {
+        return <div>Loading...</div>
+    } else if (players !== []) {
+        return (
+            <Players players={players} />
+        )
     }
-    if(player && player.data){
-      players.push(player.data.newPlayer.username)
-      setPlayers(players)
-    }
-  }, [data,player])
-
-  if(loading){
-    return <div>Loading...</div>
-  } else {
-    return <div>{!!players ? players.join(",") : "No one is here :(."}</div>
-  }
 
 };
 
