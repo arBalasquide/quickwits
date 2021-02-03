aaded1b88a17ff8bb128302fc062267214198b47
arBalasquide <adrian.balasquide@gmail.com>
2021-02-03 12:34:24 -0400

    Fixed start game button

--- web/src/components/StartButton.tsx
+++ web/src/components/StartButton.tsx
@@ -1,28 +1,40 @@
 import { useMutation } from "@apollo/client";
 import { Button } from "@chakra-ui/react";
+import { Form, Formik } from "formik";
+import { useRouter } from "next/router";
 import { StartGameDocument } from "../generated/graphql";
 
 export const StartButton = () => {
-  const [startGame, { loading }] = useMutation(StartGameDocument);
+  const [startGame] = useMutation(StartGameDocument);
+  const router = useRouter();
+
   return (
     <>
-      <form
-        onSubmit={() => {
-          startGame({});
+      <Formik
+        initialValues={{}}
+        onSubmit={async () => {
+          const response = await startGame({});
+          if (!response.data.startGame.errors) {
+            router.push("/game");
+          }
         }}
       >
-        <Button
-          colorScheme="blue"
-          m={2}
-          position="absolute"
-          top={0}
-          right={0}
-          isLoading={loading}
-          type="submit"
-        >
-          Start Game
-        </Button>
-      </form>
+        {({ isSubmitting }) => (
+          <Form>
+            <Button
+              colorScheme="blue"
+              m={2}
+              position="absolute"
+              top={0}
+              right={0}
+              isLoading={isSubmitting}
+              type="submit"
+            >
+              Start Game
+            </Button>
+          </Form>
+        )}
+      </Formik>
     </>
   );
 };
