4d3131603a334bd8ec1d35a37e6f4f866bcb931b
Adrian Rivera Balasquide <adrian.balasquide@gmail.com>
2021-02-19 18:06:18 -0400

    Update toErrorMap.ts

--- web/src/utils/toErrorMap.ts
+++ web/src/utils/toErrorMap.ts
@@ -1,6 +1,6 @@
-import { FieldMessage } from "../generated/graphql";
+import { FieldError } from "../generated/graphql";
 
-export const toErrorMap = (errors: FieldMessage[]) => {
+export const toErrorMap = (errors: FieldError[]) => {
   const errorMap: Record<string, string> = {};
   errors.forEach(({ field, message }) => {
     errorMap[field] = message;
