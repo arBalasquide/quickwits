dcb237e04947f45aa210703972e1c3e2448fec91
Adrian Rivera Balasquide <adrian.balasquide@gmail.com>
2021-02-19 18:04:54 -0400

    Workaround for bug

--- web/src/theme.tsx
+++ web/src/theme.tsx
@@ -3,19 +3,20 @@ import { createBreakpoints } from "@chakra-ui/theme-tools";
 
 const fonts = { mono: `'Menlo', monospace` };
 
-const breakpoints = createBreakpoints({
+// TODO: Fix build error
+/*const breakpoints = createBreakpoints({
   sm: "40em",
   md: "52em",
   lg: "64em",
   xl: "80em",
-});
+});*/
 
 const theme = extendTheme({
   colors: {
     black: "#16161D",
   },
   fonts,
-  breakpoints,
+  //breakpoints,
   icons: {
     logo: {
       path: (
